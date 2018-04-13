fc2fbd828fdf79d6887341970344fa61d53c7dc0
Nicolas Eckhart <nicolaseckhart@hotmail.com>
2018-04-13 18:04:21 +0200

    added Hero and HeroCollection to parse and display hero data

---
+++ src/HeroCollection.js
@@ -0,0 +1,29 @@
+// Libraries
+const rp = require('request-promise');
+
+// Classes
+const Hero = require('./Hero');
+
+class HeroCollection {
+    constructor() {
+        this.heroes = [];
+        this.parseHeroes();
+    }
+
+    async parseHeroes() {
+        let heroData = await rp('https://api.opendota.com/api/heroes');
+        heroData = JSON.parse(heroData);
+        for (let i = 0; i < heroData.length; i++) {
+            this.heroes.push(new Hero(heroData[i]));
+        }
+    }
+
+    heroNameFromNumber(number) {
+        for (let i = 0; i < this.heroes.length; i++) {
+            if (this.heroes[i].number == number) {
+                return this.heroes[i].name;
+            }
+        }
+    }
+}
+module.exports = HeroCollection;

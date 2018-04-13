9405ebb9e412dbe77b0eb6bd5a1c406b32dcabb6
Nicolas Eckhart <nicolaseckhart@hotmail.com>
2018-04-13 18:31:49 +0200

    added specs for Hero.js

---
+++ spec/Hero.spec.js
@@ -0,0 +1,24 @@
+const Hero = require('../src/Hero');
+
+describe("Hero", function () {
+    var mockData = { "id": 0, "localized_name": "foobar" }
+    var testHero;
+
+    beforeEach(function () {
+        testHero = new Hero(mockData);
+    });
+
+    describe("#number", function () {
+        it("sets the number attribute to the id of the mock data", function () {
+            let number = testHero.number;
+            expect(number).toEqual(mockData.id);
+        });
+    });
+
+    describe("#name", function() {
+        it("sets the name attribute to the localized_name of the mock data", function() {
+            let name = testHero.name;
+            expect(name).toEqual(mockData.localized_name);
+        });
+    })
+});

b994065bc2dd88bd801dad5ba5f97bf437fe0e71
Nicolas Eckhart <nicolaseckhart@hotmail.com>
2018-05-04 13:50:30 +0200

    fixed linting issues in help.js

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,17 +1,18 @@
-const Discord = require("discord.js");
-const config = require("../../config/config.json");
+const config = require('../../config/config.json');
 
-module.exports.run = async (bot, message, args) => {
-    let helpMessage = "";
-    for(let i = 0; i < bot.commands.array().length; i++) {
-        let command = bot.commands.array()[i];
-        helpMessage += `__**${command.help.name}**__\n**Usage:** \`${config.prefix}${command.help.usage}\`\n**Description:** ${command.help.description}\n\n`;
-    }
-    message.channel.send(helpMessage);
-}
+module.exports.run = (bot, message) => {
+  let helpMessage = '';
+  for (let i = 0; i < bot.commands.array().length; i++) {
+    let command = bot.commands.array()[i];
+    helpMessage += `__**${command.help.name}**__\n`;
+    helpMessage += `**Usage:** \`${config.prefix}${command.help.usage}\`\n`;
+    helpMessage += `**Description:** ${command.help.description}\n\n`;
+  }
+  message.channel.send(helpMessage);
+};
 
 module.exports.help = {
-    name: "help",
-    usage: "help",
-    description: "The bot lists all it's commands and tells you how to use them."
-}
+  name: 'help',
+  usage: 'help',
+  description: "The bot lists all it's commands and tells you how to use them.",
+};

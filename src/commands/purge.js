e93e71e901393e0ff24f804e9dfb791c12e6d63c
Nicolas Eckhart <nicolaseckhart@hotmail.com>
2018-05-04 13:47:10 +0200

    fixed linting issues in purge.js

--- src/commands/purge.js
+++ src/commands/purge.js
@@ -1,17 +1,18 @@
-const Discord = require("discord.js");
-
 module.exports.run = async (bot, message, args) => {
-    const deleteCount = parseInt(args[0], 10);
-    if (!deleteCount || deleteCount < 2 || deleteCount > 100)
-        return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
+  const deleteCount = parseInt(args[0], 10);
+  if (!deleteCount || deleteCount < 2 || deleteCount > 100) {
+    message.reply('Provide a number of messages to delete [2-100]');
+    return;
+  }
 
-    const fetched = await message.channel.fetchMessages({ count: deleteCount });
-    message.channel.bulkDelete(fetched)
-        .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
-}
+  const fetched = await message.channel.fetchMessages({ count: deleteCount });
+  message.channel.bulkDelete(fetched).catch(error => {
+    message.reply(`Couldn't delete messages because of: ${error}`);
+  });
+};
 
 module.exports.help = {
-    name: "purge",
-    usage: "purge <number>",
-    description: "The bot deletes the last <number> message in the channel you post the command in."
-}
+  name: 'purge',
+  usage: 'purge <number>',
+  description: 'The bot deletes the last <number> message in the channel you post the command in.',
+};

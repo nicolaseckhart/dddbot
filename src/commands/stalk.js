c4d03648978995d06ac4fe054344bc5e37f0f673
Nicolas Eckhart <nicolaseckhart@hotmail.com>
2018-05-04 13:56:07 +0200

    fixed linting issues in stalk.js

--- src/commands/stalk.js
+++ src/commands/stalk.js
@@ -1,16 +1,15 @@
-const Discord = require("discord.js");
-const Player = require("../Player");
+const Player = require('../Player');
 
 module.exports.run = async (bot, message, args) => {
-    for(let i = 0; i < args.length; i++) {
-        let player = new Player(args[i], bot.heroes);
-        await player.parsePlayerInformation();
-        message.channel.send(player.embedify());
-    }
-}
+  for (let i = 0; i < args.length; i++) {
+    let player = new Player(args[i], bot.heroes);
+    await player.parsePlayerInformation();
+    message.channel.send(player.embedify());
+  }
+};
 
 module.exports.help = {
-    name: "stalk",
-    usage: "stalk <opendota-player-id>", 
-    description: "The bot posts a summary of the players OpenDota profile in the channel."
-}
+  name: 'stalk',
+  usage: 'stalk <opendota-player-id>',
+  description: 'The bot posts a summary of the players OpenDota profile in the channel.',
+};
